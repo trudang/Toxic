b7303b207df58d11530e472141ed018deeef0a70
trudang <trudang93@gmail.com>
2014-01-19 23:23:17 -0500

    text_kernel_functions_db-text

---
+++ test_kernel_functions_db-text.py
@@ -0,0 +1,113 @@
+import numpy as np
+import itertools
+import copy
+import matplotlib.pyplot as plt
+import pylab as P
+
+from nltk.corpus import wordnet
+from src.nlp.SemanticString import SemanticString
+from src.nlp.SemanticWord import SemanticWord
+
+txt_name = 'db-text.txt'
+with open(txt_name) as f:
+  strings = [tweet.strip() for tweet in f.readlines()]
+
+all_words = [token.word for string in strings for token in SemanticString(string, {}).tokens]
+
+# print len(all_words)
+
+words = [word for word in all_words if SemanticWord(word,'NN',{}).synset != None]
+
+# print len(words)
+
+print "Words used:", words
+
+senses = [list(set([sense.name[:sense.name.index('.')] for sense in SemanticWord(word,'NN',{}).synset])) for word in words]
+
+all_senses = np.sum(senses)
+
+print "List of unique senses:", all_senses
+
+# freq = [list(np.random.poisson(100,len(sense))) for sense in senses]
+# freq = [[91, 96, 103], [100, 108, 107, 93, 100, 104, 97]]
+
+# print freq
+
+# all_freq = np.sum(freq)
+
+# print "List of frequencies:", all_freq
+# print "Total frequencies:", sum(all_freq)
+
+# expected_kernel = {word: dict(zip(senses[i], freq[i])) for i,word in enumerate(words)}
+
+# print "Expected kernel:"
+# print expected_kernel
+
+corpus = words[:]
+
+def make_freq_dict(word_list, corpus):
+	senses = [list(set([sense.name[:sense.name.index('.')] for sense in SemanticWord(word,'NN',{}).synset])) for word in words]
+	freq = [list(np.zeros(len(sense))) for sense in senses]
+	dic = {word: dict(zip(senses[i], freq[i])) for i,word in enumerate(words)}
+	for element in corpus:
+		for word in dic.keys():
+			if element in dic[word].keys():
+				dic[word][element] += 1
+	return dic
+
+def normalize_control(dic):
+	new_dic = copy.deepcopy(dic)
+	sum_of_dic = float(sum(np.sum([word.values() for word in dic.values()])))
+	# print sum_of_dic
+	for word in new_dic:
+		for sense in new_dic[word]:
+			new_dic[word][sense] /= sum_of_dic
+	return new_dic
+
+def normalize_dict(dic):
+	words = dic.keys()
+	senses = [dic[word].keys() for word in dic.keys()]
+	values = [dic[word].values() for word in dic.keys()]
+	sum_of_dic = sum(np.sum(values))
+	# print max(np.sum(values))
+	# print sum_of_dic
+	new_values = [list(np.array(v) / float(sum_of_dic)) for v in values]
+	return {word: dict(zip(senses[i], new_values[i])) for i,word in enumerate(words)}
+
+def histogram(kernel):
+	words = kernel.keys()
+	senses = [kernel[word].keys() for word in kernel.keys()]
+	values = [kernel[word].values() for word in kernel.keys()]
+	fig = plt.figure()
+	n, bins, patches = plt.hist(values, 50, 
+		#normed=1.0, 
+		histtype='bar', label=words)
+	plt.xlabel("Frequency of Sense")
+	plt.ylabel("Number of Senses with Same Frequency")
+	# plt.legend()
+	plt.show()
+
+kernel = make_freq_dict(words, corpus)
+
+print "Actual kernel:"
+print kernel
+
+# print "Actual kernel is same as expected:", kernel == expected_kernel
+
+sum_kernel = sum(np.sum([word.values() for word in kernel.values()]))
+
+# print "Total frequencies:", sum_kernel, "; Same as expected:", sum_kernel == sum(all_freq)
+
+print "Positive control normalization:"
+control_norm = normalize_control(kernel)
+print control_norm
+
+print "Normalization with dict as input:"
+dict_norm = normalize_dict(kernel)
+print dict_norm
+
+print "Normalization works?", dict_norm == control_norm
+
+print "Total of values in normalized kernel:", sum(np.sum([word.values() for word in dict_norm.values()]))
+
+histogram(kernel)
